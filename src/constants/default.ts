88ca7f1394e2ea6808729acb6876a4b3cf81777a
pashaman <plavel@gmail.com>
2020-09-11 10:55:08 +0700

    [refactroing] renamed config properties

--- src/constants/default.ts
+++ src/constants/default.ts
@@ -1,9 +1,9 @@
 import { EDumpInterval } from "./EDumpInterval";
 
-export const DEFAULT_DI_DB_PORT: number = 34567;
-export const DEFAULT_DI_DB_WORKERS_COUNT: number = 10;
-export const DEFAULT_DI_DB_DUMP_CREATE_INTERVAL: EDumpInterval = EDumpInterval.DAY;
-export const DEFAULT_DI_DB_DUMPS_DIRECTORY: string = '../../data';
-export const DEFAULT_DI_DB_LOGS_DIRECTORY: string =  '../../logs/';
-export const DEFAULT_DI_DB_DUMP_CURRENT_NAME: string =  'current.json';
-export const DEFAULT_DI_DB_LOGS_TTL: string = '7d';
+export const DEFAULT_DIANA_DB_PORT: number = 34567;
+export const DEFAULT_DIANA_DB_WORKERS_COUNT: number = 10;
+export const DEFAULT_DIANA_DB_DUMP_CREATE_INTERVAL: EDumpInterval = EDumpInterval.DAY;
+export const DEFAULT_DIANA_DB_DUMPS_DIRECTORY: string = '../../data';
+export const DEFAULT_DIANA_DB_LOGS_DIRECTORY: string =  '../../logs/';
+export const DEFAULT_DIANA_DB_DUMP_CURRENT_NAME: string =  'current.json';
+export const DEFAULT_DIANA_DB_LOGS_TTL: string = '7d';
